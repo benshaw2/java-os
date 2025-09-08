14adaf1661a7337cdfd16f78cac1d48b3581d8f4
benshaw2 <bendougshaw@gmail.com>
2025-09-08 04:09:42 -0600

    Initial commit of the project

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Welcome to JavaOS!");
+        while (true) {
+            Thread.sleep(1000);
+            System.out.println("Running...");
+        }
+    }
+}
